3541c828a9927f8a3d9f44cd12ea82f16da7d1c3
carl-chua <carlchua0701@gmail.com>
2024-06-10 01:57:16 +0800

    feat: add get original url

--- src/main/java/com/example/split_smart/controller/UrlController.java
+++ src/main/java/com/example/split_smart/controller/UrlController.java
@@ -3,12 +3,14 @@ package com.example.split_smart.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.split_smart.service.UrlService;
 
 @RestController
+@RequestMapping("/url")
 public class UrlController {
 
   private final UrlService urlService;
@@ -21,9 +23,9 @@ public class UrlController {
     this.urlService = urlService;
   }
 
-  @GetMapping("/abc")
-  public String abc() {
-    return "abc";
+  @GetMapping("/get_original_url")
+  public String getOriginalUrl(@RequestParam(value = "url", required = true) String url) {
+    return urlService.getOriginalUrl(url);
   }
 
   @PostMapping("/shorten")
--- src/main/java/com/example/split_smart/service/UrlService.java
+++ src/main/java/com/example/split_smart/service/UrlService.java
@@ -1,5 +1,7 @@
 package com.example.split_smart.service;
 
+import java.lang.reflect.Type;
+import java.util.List;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -9,9 +11,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.example.split_smart.model.Url;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 @Service
 public class UrlService {
@@ -29,6 +33,32 @@ public class UrlService {
   private Random random = new Random();
 
   private RestTemplate restTemplate = new RestTemplate();
+
+  public String getOriginalUrl(String originalUrl) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.set("apikey", supabaseKey);
+    headers.set("Authorization", "Bearer " + supabaseKey);
+    headers.set("Range", "0-9");
+
+    HttpEntity<String> entity = new HttpEntity<>(null, headers);
+
+    // Build the URL with the query parameter
+    String urlWithQueryParam = UriComponentsBuilder.fromHttpUrl(supabaseUrl + "/rest/v1/url")
+        .queryParam("original_url", "eq." + originalUrl)
+        .toUriString();
+
+    ResponseEntity<String> response = restTemplate.exchange(urlWithQueryParam, HttpMethod.GET, entity,
+        String.class);
+    if (response.getStatusCode().is2xxSuccessful()) {
+      Gson gson = new Gson();
+      Type urlListType = new TypeToken<List<Url>>() {
+      }.getType();
+      List<Url> urls = gson.fromJson(response.getBody(), urlListType);
+      return urls.isEmpty() ? "" : urls.get(0).getOriginalUrl();
+    } else {
+      return "";
+    }
+  }
 
   public String createShortUrl(String originalUrl) {
     String shortUrl = generateRandomShortUrl();
